d6acd9e4a8dd9bbb227760aa77d6f81c90abe3d0
Amit Patel <punit.dwivedi@velocis.co.in>
2020-08-24 19:47:50 +0530

    Approver API

--- src/main/java/org/egov/bookings/controller/MasterController.java
+++ src/main/java/org/egov/bookings/controller/MasterController.java
@@ -5,17 +5,24 @@ import java.util.List;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.egov.bookings.common.model.ResponseModel;
+import org.egov.bookings.contract.BookingApprover;
 import org.egov.bookings.contract.DocumentFields;
+import org.egov.bookings.contract.OsbmApproverRequest;
 import org.egov.bookings.model.InventoryModel;
+import org.egov.bookings.model.OsbmApproverModel;
+import org.egov.bookings.service.BookingsService;
 import org.egov.bookings.service.MasterService;
 import org.egov.bookings.validator.BookingsFieldsValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class MasterController.
  */
@@ -29,6 +36,14 @@ public class MasterController {
 	/** The master service. */
 	@Autowired
 	private MasterService masterService;
+	
+	/** The bookings fields validator. */
+	@Autowired
+	BookingsFieldsValidator bookingsFieldsValidator;
+	
+	/** The bookings service. */
+	@Autowired
+	private BookingsService bookingsService;
 	
 	/**
 	 * Gets the park community inventory details.
@@ -54,6 +69,61 @@ public class MasterController {
 			e.printStackTrace();
 		}
 		return ResponseEntity.ok(inventoryModelList);
+	}
+	
+	
+	/**
+	 * Creates the osbm approver.
+	 *
+	 * @param osbmApproverRequest the osbm approver request
+	 * @return the response entity
+	 */
+	@PostMapping("/approver/_create")
+	public ResponseEntity<?> createOsbmApprover(@RequestBody OsbmApproverRequest osbmApproverRequest) {
+		
+		if (BookingsFieldsValidator.isNullOrEmpty(osbmApproverRequest)) 
+		{
+			throw new IllegalArgumentException("Invalid osbmApproverRequest");
+		}
+		ResponseModel rs = new ResponseModel();
+		try {
+			bookingsFieldsValidator.validateOsbmApproverBody(osbmApproverRequest);
+			OsbmApproverModel osbmModel = masterService.createOsbmApprover(osbmApproverRequest);
+			rs.setStatus("200");
+			rs.setMessage("Data submitted in osbm table");
+			rs.setData(osbmModel);
+		}
+		catch(Exception e)
+		{
+			LOGGER.error("Exception occur in the createOsbmApprover " + e);
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok(rs);
+
+	}
+	
+	
+	/**
+	 * Fetch all approver.
+	 *
+	 * @return the response entity
+	 */
+	@GetMapping("all/approver/_fetch")
+	public ResponseEntity<?> fetchAllApprover() {
+		ResponseModel rs = new ResponseModel();
+		try {
+			List<BookingApprover> bookingApproverList = masterService.fetchAllApprover();
+			rs.setStatus("200");
+			rs.setMessage("Data submitted successfully");
+			rs.setData(bookingApproverList);
+		}
+		catch(Exception e)
+		{
+			LOGGER.error("Exception occur in the fetchAllApprover " + e);
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok(rs);
+
 	}
 	
 }
--- src/main/java/org/egov/bookings/service/MasterService.java
+++ src/main/java/org/egov/bookings/service/MasterService.java
@@ -2,8 +2,12 @@ package org.egov.bookings.service;
 
 import java.util.List;
 
+import org.egov.bookings.contract.BookingApprover;
+import org.egov.bookings.contract.OsbmApproverRequest;
 import org.egov.bookings.model.InventoryModel;
+import org.egov.bookings.model.OsbmApproverModel;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Interface MasterService.
  */
@@ -17,5 +21,20 @@ public interface MasterService {
 	 * @return the park community inventory details
 	 */
 	public List< InventoryModel > getParkCommunityInventoryDetails(String venue, String sector);
+	
+	/**
+	 * Creates the osbm approver.
+	 *
+	 * @param osbmApproverRequest the osbm approver request
+	 * @return the osbm approver model
+	 */
+	public OsbmApproverModel createOsbmApprover(OsbmApproverRequest osbmApproverRequest);
+	
+	/**
+	 * Fetch all approver.
+	 *
+	 * @return the list
+	 */
+	public List<BookingApprover> fetchAllApprover();
 
 }
