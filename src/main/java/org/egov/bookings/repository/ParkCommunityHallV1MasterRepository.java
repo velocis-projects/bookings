9af10abbbdbf64de192505996709cdd07cb9f277
velocis-projects <punit.dwivedi@velocis.co.in>
2020-08-29 23:49:13 +0530

    assignee and master

---
+++ src/main/java/org/egov/bookings/repository/ParkCommunityHallV1MasterRepository.java
@@ -0,0 +1,17 @@
+package org.egov.bookings.repository;
+
+import org.egov.bookings.model.ParkCommunityHallV1MasterModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The Interface ParkCommunityHallV1MasterRepository.
+ */
+@Repository
+public interface ParkCommunityHallV1MasterRepository extends JpaRepository<ParkCommunityHallV1MasterModel, String> {
+
+	ParkCommunityHallV1MasterModel findById(String bookingVenue);
+
+	ParkCommunityHallV1MasterModel findByVenueTypeAndSector(String bookingVenue, String sector);
+
+}
