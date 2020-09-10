fcf814c1919544696aa665a361129a9d5348d825
Amit Patel <punit.dwivedi@velocis.co.in>
2020-09-10 16:11:48 +0530

    Persister functionality of master

--- src/main/java/org/egov/bookings/config/BookingsConfiguration.java
+++ src/main/java/org/egov/bookings/config/BookingsConfiguration.java
@@ -86,6 +86,18 @@ public class BookingsConfiguration {
 
 	@Value("${kafka.topics.update.approver.service}")
 	private String updateApproverTopic;
+	
+	@Value("${kafka.topics.save.osbm.fee.service}")
+	private String saveOsbmFeeTopic;
+
+	@Value("${kafka.topics.update.osbm.fee.service}")
+	private String updateOsbmFeeTopic;
+	
+	@Value("${kafka.topics.save.osujm.fee.service}")
+	private String saveOsujmFeeTopic;
+
+	@Value("${kafka.topics.update.osujm.fee.service}")
+	private String updateOsujmFeeTopic;
 
 	@Value("${kafka.topics.notification.sms}")
 	private String smsNotifTopic;
