dbee951855b9db9bed60038265ddc102aa55a54d
Amit Patel <punit.dwivedi@velocis.co.in>
2020-09-11 13:38:59 +0530

    Added master persister API

--- src/main/java/org/egov/bookings/contract/MasterRequest.java
+++ src/main/java/org/egov/bookings/contract/MasterRequest.java
@@ -3,10 +3,6 @@ package org.egov.bookings.contract;
 import java.io.Serializable;
 import java.util.List;
 
-import org.egov.bookings.model.OsbmApproverModel;
-import org.egov.bookings.model.OsbmFeeModel;
-import org.egov.bookings.model.OsujmFeeModel;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
@@ -25,14 +21,14 @@ public class MasterRequest implements Serializable {
 	
 	/** The approver list. */
 	@JsonProperty("ApproverDetails")
-	private List<OsbmApproverModel> approverList;
+	private List<CommonMasterFields> approverList;
 	
 	/** The osbm fee list. */
 	@JsonProperty("OsbmFeeDetails")
-	private List<OsbmFeeModel> osbmFeeList;
+	private List<CommonMasterFields> osbmFeeList;
 	
 	/** The osujm fee list. */
 	@JsonProperty("OsujmFeeDetails")
-	private List<OsujmFeeModel> osujmFeeList;
+	private List<CommonMasterFields> osujmFeeList;
 	
 }
